5b69b3adca897b4ac756597353d122c4830cb16b
fenglibin <fenglibin@agora.io>
2021-04-02 14:52:12 +0800

    jitpack test

--- app/src/main/java/com/aderan/projectpractice/MainActivity.kt
+++ app/src/main/java/com/aderan/projectpractice/MainActivity.kt
@@ -2,10 +2,13 @@ package com.aderan.projectpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.agora.netless.library.Test
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Test.add(1, 2)
     }
 }
---
+++ library/src/main/java/com/agora/netless/library/Test.java
@@ -0,0 +1,7 @@
+package com.agora.netless.library;
+
+public class Test {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
