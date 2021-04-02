edc4810d7d93600cbc8acb1e79123e2849d23170
fenglibin <fenglibin@agora.io>
2021-04-02 17:37:01 +0800

    ci  test

--- app/src/main/java/com/aderan/projectpractice/MainActivity.kt
+++ app/src/main/java/com/aderan/projectpractice/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.aderan.projectpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.agora.netless.library.Test
 
 class MainActivity : AppCompatActivity() {
